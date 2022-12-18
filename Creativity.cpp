46e2977b153bf1ace79a38c3cab7d08270783b44
SOBAN7AKRAM <120157492+SOBAN7AKRAM@users.noreply.github.com>
2022-12-18 15:00:38 +0500

    Add files via upload

---
+++ Creativity.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+main () {
+cout << "                                         " << endl;
+cout << "                        /$$$$       $$$$\\    " << endl;
+cout << "                      /       $$$$$       \\     " << endl;
+cout << "                    /                       \\    " << endl;
+cout << "              ******       C C C  RRR        ******      " << endl;
+cout << "             *           C        R R              *        " << endl;
+cout << "            *    * *    C         RR         * *    *        " << endl;
+cout << "           *    *  *     C        R R        *  *    *     " << endl;
+cout << "          *    *   *       C C C  R   R      *   *    *  " << endl;
+cout << "         *____*    *                         *    *____*    " << endl;
+cout << "                   *       777777777         *             " << endl;
+cout << "                   *               7         *           " << endl;
+cout << "                   *              7          *        " << endl;
+cout << "                   *             7           *        " << endl;
+cout << "                   *            7            *           " << endl;
+cout << "                   *           7             *               " << endl;
+cout << "                   *          7              *             " << endl;
+cout << "                   *         7               *     " << endl;
+cout << "                   *                         *      " << endl;
+cout << "                   *        *********        *       " << endl;
+cout << "                   *        *********        *        " << endl;
+cout << "                   ***************************        " << endl;}
---
+++ ProjectHeader.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+main () { 
+cout << "*******************************************" << endl;
+cout << "*                                         *" << endl;
+cout << "*                                         *" << endl;
+cout << "*      UNIVERSITY OF ENGINEERING          *" << endl; 
+cout << "*          AND TECHNOLOGY                 *" << endl; 
+cout << "*            LAHORE                       *" << endl; 
+cout << "*                                         *" << endl; 
+cout << "*                                         *" << endl; 
+cout << "*                                         *" << endl; 
+cout << "*******************************************";}   
